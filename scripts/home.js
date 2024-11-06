c4964063d6aa8aa6d1ae9b7b877fbd298a54f963
Mitchell Else <elsemitchell1@gmail.com>
2024-11-05 21:31:06 -0700

    footer

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,24 @@
+//typewriter function
+function typeWords(element){
+    const prefix = "I am a, ";
+    let wordArr = ["Web Developer", "Problem Solver", "Tech Enthusiast", "Hard Worker"];
+    let wordIndex = 0;
+    let charIndex = 0;
+    function type(){
+        if(charIndex < wordArr[wordIndex].length){
+            element.innerHTML = prefix + wordArr[wordIndex].substring(0, charIndex + 1);
+            charIndex++;
+            setTimeout(type, 300);
+        } else {
+            setTimeout(() => {
+                charIndex = 0;
+                wordIndex = (wordIndex + 1) % wordArr.length;
+                setTimeout(type, 800)
+            }, 1000);
+        }
+    }
+    type()
+}
 //fetch hero component
 let mainSection = document.getElementById("main-content");
 fetch("../htmlcomponents/hero.html")
@@ -11,16 +32,18 @@ fetch("../htmlcomponents/hero.html")
         function createHero(template, headingText, secondHeading, paragraph, image, alt, reverse){
             template.querySelector(".hero-container").style.flexDirection = reverse ? "row-reverse" : "row";
             template.querySelector(".hero-heading").innerHTML = headingText;
-            template.querySelector(".hero-secondHeading").innerHTML = secondHeading;
+            template.querySelector(".hero-secondHeading-container").innerHTML += secondHeading ? "<span class='cursor'></span>" : "";
+            const subHeading = template.querySelector(".hero-secondHeading");
             template.querySelector(".hero-paragraph").innerHTML = paragraph;
             const imgElement = template.querySelector(".hero-img");
             imgElement.setAttribute("src", image);
             imgElement.setAttribute("alt", alt);
             mainSection.appendChild(template);
+            secondHeading && typeWords(subHeading);
         }
 
         var hero1Heading = "Hi, I am <br/> Mitchell Else";
-        var hero1SecondHeading = "I am a, Web Developer";
+        var hero1SecondHeading = true;
         var hero1Paragraph = 
         `I am a dedicated web developer currently halfway through 
         my computer science degree. Known for being hardworking and 
@@ -32,7 +55,7 @@ fetch("../htmlcomponents/hero.html")
         var hero1Alt = "Family Photo";
 
         var hero2Heading = "Backstory";
-        var hero2SecondHeading = "";
+        var hero2SecondHeading = false;
         var hero2Paragraph = 
         `Originally from Australia, I now reside in Edmonton AB, Canada. 
         In Australia I was a trade qualified heavy metal fabricator. 
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,25 @@
 //navbar html fetch
-fetch("../navbar.html")
+fetch("../htmlcomponents/navbar.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("navbar").innerHTML = data;
+        
+        const currentPage = window.location.pathname.split("/").pop();
+        const links = document.querySelectorAll("nav a");
+        links.forEach(link => {
+            if(link.getAttribute("href")=== currentPage){
+                link.classList.add("nav-active","text-gradient");
+            }else{
+                link.style.color = "white";
+            }
+        })
+    });
+
+//footer html fetch
+fetch("../htmlcomponents/footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer").innerHTML = data;
         
         const currentPage = window.location.pathname.split("/").pop();
         const links = document.querySelectorAll("nav a");
